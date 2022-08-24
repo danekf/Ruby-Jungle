ab191e7e156072f08b0e0247333ec810035081b1
Danek <danek.fill@outlook.com>
2022-08-24 13:32:25 -0400

    Add completed item list to orders, bracket notation was not working, so switched to dot notation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,7 @@
 class OrdersController < ApplicationController
 
   def show
-    @order = Order.find(params[:id])
-    @items = LineItem.where("order_id = ?", params[:id])
+    @order = Order.find(params[:id])    
   end
 
   def create
