ea6c872e40b34948c163e7d3ef03ecf4767adbb3
Danek <danek.fill@outlook.com>
2022-08-25 13:53:46 -0400

    Add new category admin page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,4 +3,22 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new 
+    @category = Category.new
+  end
+
+  def create 
+    @category = Category.new(category_params)
+  
+  
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
 end
