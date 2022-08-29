b8da4705b6d0e639bee69824b5a2b45c9dcbce84
Danek <danek.fill@outlook.com>
2022-08-29 19:28:59 +0000

    Add user model validations before testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :email, presence: true
+  validates :email, uniqueness: =>{:case_sensitive => false}
+
+  validates :passsword, presence: true, length: {minumum: 6}
+  validates :passsword_confirmation, presence: true, length: {minumum: 6}
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @user = User.new
+  end
+
+  describe 'User Validations' do
+    it 'saves when all data is present' do
+      @valid_user = User.new(first_name: 'D', last_name: 'F', email: 'unique@email.com', password: 'securePassword', passsword_confirmation: 'securePassword')
+      expect(@valid_user).to be_valid
+    end
+
+    it 'has a password' do
+      skip
+    end
+
+    it 'has a confirmed passsword' do
+      skip
+    end
+
+    it 'has the same password and confirmed password' do
+      skip
+    end
+
+    it 'has an email' do
+      skip
+    end
+
+    it 'has an email not in the db (db is not case sensitive)' do
+      skip
+    end
+
+    it 'has a first name' do
+      skip
+    end
+
+    it 'has a last name' do
+      skip
+    end
+
+  end
 end
